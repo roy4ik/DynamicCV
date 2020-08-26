772ed44b9ebd482b1a66edd08dd975761c88c2c3
daniel.ach <MrAch26@users.noreply.github.com>
2020-08-26 16:40:46 +0300

    about.js + bg-img + changes in html and css

--- js/about.js
+++ js/about.js
@@ -1,1 +1,14 @@
 console.log("this is about js");
+
+let main = document.getElementsByTagName("main")[0];
+let newDiv = document.createElement("div");
+newDiv.style.backgroundColor = "var(--main-color)";
+newDiv.style.width = "100%";
+newDiv.style.height="70vh"
+main.appendChild(newDiv)
+
+newDiv.innerHTML = "<img id='logo' src='img/logo.svg' alt='logo'>"
+let logo = document.getElementById("logo");
+logo.style.height="40vh";
+logo.style.marginTop="5vh";
+logo.style.marginLeft="5vw";
