238948b804446266019625eef814772211aab459
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-08-26 20:08:47 +0300

    changed load.js to toggle display instead of script load, because cannot clear memory of already loaded script. added div to main with class of section to toggle display. changed parent in about.js

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 console.log("this is about js");
-// main
-let main = document.getElementsByTagName("main")[0];
+// // main
+// let main = document.getElementsByTagName("main")[0];
 
 // div
 let container1 = document.createElement("div");
@@ -9,21 +9,21 @@ let container2 = document.createElement("div");
 container1.style.backgroundColor = "var(--third-color)";
 container2.style.backgroundColor = "var(--third-color)";
 
-container1.style.color="var(--main-bg-color)"
-container2.style.color="var(--main-bg-color)"
+container1.style.color = "var(--main-bg-color)"
+container2.style.color = "var(--main-bg-color)"
 
 container1.style.width = "100%";
 container2.style.width = "100%";
 
-container1.style.marginBottom="20px"
+container1.style.marginBottom = "20px"
 
-container1.style.display="inline-flex"
-container1.id="container1"
-container2.style.display="inline-flex"
-container2.id="container2"
+container1.style.display = "inline-flex"
+container1.id = "container1"
+container2.style.display = "inline-flex"
+container2.id = "container2"
 
-main.appendChild(container1);
-main.appendChild(container2);
+AboutSectionContainer.appendChild(container1);
+AboutSectionContainer.appendChild(container2);
 
 
 let newDiv1 = document.createElement("div");
@@ -37,28 +37,25 @@ container1.appendChild(newDiv2);
 container2.appendChild(newDiv3);
 container2.appendChild(newDiv4);
 
-newDiv2.style.margin="20px"
+newDiv2.style.margin = "20px"
 
 newDiv1.innerHTML = "<img id='logo' src='img/logo.svg' alt='logo'>"
-newDiv2.innerHTML="\
+newDiv2.innerHTML = "\
 <h1>About</h1>\
 <p>Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!orem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa! </p>"
 
 newDiv4.innerHTML = "<img id='logo1' src='img/logo.svg' alt='logo'>"
-newDiv3.innerHTML="\
+newDiv3.innerHTML = "\
 <h1>About</h1>\
 <p>Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!orem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa! </p>"
 
 
 let logo = document.getElementById("logo");
-logo.style.height="50vh";
-logo.style.marginTop="10vh";
-logo.style.marginLeft="5vw";
+logo.style.height = "50vh";
+logo.style.marginTop = "10vh";
+logo.style.marginLeft = "5vw";
 
 let logo1 = document.getElementById("logo1");
-logo1.style.height="50vh";
-logo1.style.marginTop="10vh";
-logo1.style.marginLeft="5vw";
-
-
-
+logo1.style.height = "50vh";
+logo1.style.marginTop = "10vh";
+logo1.style.marginLeft = "5vw";
