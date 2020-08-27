3eb1d06c083b48a65c4305ad4817db0c0a9a1703
daniel.ach <MrAch26@users.noreply.github.com>
2020-08-27 19:55:05 +0300

    ok

--- js/about.js
+++ js/about.js
@@ -1,31 +1,29 @@
 console.log("this is about js");
 
-// // main
-// let main = document.getElementsByTagName("main")[0];
+//any content needs to be appended into aboutSectionContainer declared in loadSections.js
 
-// div
+// creating content containers
 let aboutContainer1 = document.createElement("div");
 let aboutContainer2 = document.createElement("div");
 let aboutContainer3 = document.createElement("div");
 
-
+// setting container styles 
 aboutContainer1.style.backgroundColor = "var(--third-color)";
 aboutContainer2.style.backgroundColor = "var(--third-color)";
 aboutContainer3.style.backgroundColor = "var(--third-color)";
 
 aboutContainer1.style.color = "var(--main-bg-color)";
-// aboutContainer2.style.color="var(--main-bg-color)";
-// aboutContainer2.style.color="var(--main-bg-color)";
-
-aboutContainer1.style.color = "var(--main-bg-color)"
-aboutContainer2.style.color = "var(--main-bg-color)"
 
 aboutContainer1.style.width = "100%";
-aboutContainer1.style.height = "85vh";
+aboutContainer1.style.height = "85%";
 aboutContainer2.style.width = "100%";
-aboutContainer2.style.height = "50vh";
+aboutContainer2.style.height = "fit-content";
+aboutContainer2.style.height = "22em";
+
 aboutContainer3.style.width = "100%";
-aboutContainer3.style.height = "45vh";
+aboutContainer3.style.height = "fit-content";
+aboutContainer3.style.height = "22em";
+
 
 
 aboutContainer1.style.marginBottom = "20px"
@@ -34,42 +32,64 @@ aboutContainer1.style.display = "inline-flex"
 aboutContainer2.style.display = "inline-flex"
 aboutContainer3.style.display = "inline-flex";
 
+aboutContainer1.style.display = "inline-flex"
+aboutContainer2.style.display = "inline-flex"
+
+//add id to containers
 aboutContainer1.id = "aboutContainer1";
 aboutContainer2.id = "aboutContainer2";
 aboutContainer3.id = "aboutContainer3";
 
 
-aboutContainer1.style.display = "inline-flex"
-aboutContainer2.style.display = "inline-flex"
-
+//appending container into the aboutSectionContainer
 aboutSectionContainer.appendChild(aboutContainer1);
 aboutSectionContainer.appendChild(aboutContainer2);
 aboutSectionContainer.appendChild(aboutContainer3);
 
 
-
+// creating elements for content containers
 let aboutNewDiv1 = document.createElement("div");
 let aboutNewDiv2 = document.createElement("div");
 
 let aboutNewDiv3 = document.createElement("div");
 let aboutNewDiv4 = document.createElement("div");
 
+//appending elements for content containers
 aboutContainer1.appendChild(aboutNewDiv1);
 aboutContainer1.appendChild(aboutNewDiv2);
-// aboutContainer2.appendChild(aboutNewDiv3);
-// aboutContainer2.appendChild(aboutNewDiv4);
 
 
 
+// adding content and styles to content
 aboutNewDiv2.style.margin = "20px"
 
 aboutNewDiv1.innerHTML = "<img id='aboutLogo' src='img/logo.svg' alt='logo'>"
 aboutNewDiv2.innerHTML = "\
 <h1>About</h1>\
-<p>Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!orem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa! </p>"
+<p>Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!orem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed, sit tempora inventore quos autem sapiente optio reprehenderit maxime commodi nobis mollitia saepe facilis accusantium vero sequi illum soluta ipsa! </p>\
+<section class='white-section' id='features'>\
+    <div class='container-fluid'>\
+      <div class='row row-features'>\
+        <div class='col-lg-4'>\
+          <i class='fas fa-check-circle icon-features fa-4x'></i>\
+          <h3 class='h3-features'>Easy to use.</h3>\
+          <p class='p-features'>So easy to use, even your dog could do it.</p>\
+        </div>\
+        <div id='bullseye' class='col-lg-4'>\
+          <i class='fas fa-bullseye icon-features fa-4x'></i>\
+          <h3 class='h3-features'>Elite Clientele</h3>\
+          <p class='p-features'>We have all we want , the greatest CV.</p>\
+        </div>\
+        <div class='col-lg-4'>\
+          <i class='fas fa-heart icon-features fa-4x'></i>\
+          <h3 class='h3-features'>Guaranteed to work.</h3>\
+          <p class='p-features'>Find the love of your CV life or your money back.</p>\
+        </div>\
+      </div>\
+    </div>\
+  </section>";
 
-// aboutNewDiv4.innerHTML = "";
-// aboutNewDiv3.innerHTML = "";
+
 
 aboutContainer2.innerHTML = "\
 <div id='card-roy' class='card mb-3' style='max-width: 50%;'>\
@@ -103,6 +123,6 @@ aboutContainer3.innerHTML = "\
 </div>"
 
 let aboutLogo = document.getElementById("aboutLogo");
-aboutLogo.style.height = "50vh";
-aboutLogo.style.marginTop = "10vh";
-aboutLogo.style.marginLeft = "5vw";
+aboutLogo.style.height = "26em";
+aboutLogo.style.marginTop = "4em";
+aboutLogo.style.marginLeft = "4em";
